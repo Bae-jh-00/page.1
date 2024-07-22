71842439a27c79bd511d6fed2aa426cd7fa7d826
Bae-jh-00 <bjh8072@gmail.com>
2024-07-22 15:04:59 +0900

    Delete HOME.js

--- HOME.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('clearButton').addEventListener('click', function() {
-    document.getElementById('textInput1').value = '';
-    document.getElementById('textInput2').value = '';
-    document.getElementById('textInput3').value = '';
-});
